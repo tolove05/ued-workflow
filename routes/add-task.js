c6d4cb6ddd78bb6331e51b7a0731349a90c2cd4e
song <xiongsongsong@outlook.com>
2013-05-24 16:26:26 +0800

    能够批量保存任务单了

--- routes/add-task.js
+++ routes/add-task.js
@@ -15,15 +15,23 @@ function trans(s) {
 
 app.post('/add-task', function (req, res) {
 
-    console.log(req.body);
     var serverInfo = {
         err: []
     };
+
+    res.header('content-type', 'text/plain;charset=utf-8');
+
+    if (!require('./login').isLogin(req)) {
+        serverInfo.err.push('请先登陆')
+        res.json(serverInfo);
+        return;
+    }
 
     try {
         var json = JSON.parse(req.body.json);
     } catch (e) {
         serverInfo.err.push('提交的数据格式非法');
+        res.json(serverInfo);
         return;
     }
 
@@ -53,32 +61,27 @@ app.post('/add-task', function (req, res) {
         if (task.to.length < 1) arr.push('您必须指定任务的完成者');
         if (task.name.length < 1) arr.push('任务名称不能为空');
         if (task.demand_side.length < 1) arr.push('缺少需求方名称');
-        if (/^[^0]\d*$/ === false) arr.push('任务时常为空或不正确');
+        task.timer = parseInt(task.timer, 10);
+        if (isNaN(task.timer) || task.timer < 1) arr.push('任务时长必须大于0');
         if (task.type.length < 1) arr.push('缺少任务类型');
 
         if (arr.length < 1) {
             TASK.push(task)
         } else {
-            serverInfo.err.push(item + '存在问题' + arr.join(','));
+            serverInfo.err.push(item + '存在问题：' + arr.join(','));
         }
     });
 
-    res.header('content-type', 'text/plain;charset=utf-8');
-
-    res.json({task: TASK});
-
-    console.log(TASK)
-    return;
-    if (!require('./login').isLogin(req)) {
-        res.end('请先登录');
+    //如果任意一条数据存在错误，则拒绝保存
+    if (serverInfo.err.length > 0) {
+        res.json(serverInfo);
         return;
     }
 
-    var body = req.body;
     var user = new DB.Collection(DB.Client, 'user');
     user.findOne({_id: DB.mongodb.ObjectID(req.session._id)}, function (err, data) {
         if (err) {
-            res.end('出错');
+            res.end('您没有权限添加任务单');
             return;
         }
 
@@ -87,33 +90,12 @@ app.post('/add-task', function (req, res) {
             return;
         }
 
-        var task = {
-            //任务名
-            name: trans(body.name),
-            //给谁,记录ObjectId
-            to: trans(body.to),
-            //需求方
-            demand_side: trans(body.demand_side),
-            //小时数
-            timer: trans(body.timer),
-            //任务类型
-            note: trans(body.note),
-            //派发者，记录ObjectId
-            from: req.session._id,
-            //时间戳
-            time_stamp: Date.now()
-        };
-
-        //任务名称，指派者，被指派者必须存在
-        if (!task.name || !task.to || !task.from) {
-            res.end('参数错误');
-            return;
-        }
-
         var collection = new DB.Collection(DB.Client, 'task');
-        collection.insert(task, {safe: true},
+        collection.insert(TASK, {safe: true},
             function () {
-                res.end('保存成功');
+                serverInfo.msg = '保存成功';
+                serverInfo.success = true;
+                res.json(serverInfo);
             });
     });
 
